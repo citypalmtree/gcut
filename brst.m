cf09f37b1d4ca0ef345931881a253cbcdf1dd5c9
Karl Marrett <kdmarrett@gmail.com>
2018-11-08 09:48:24 -0800

    qfx

--- brst.m
+++ brst.m
@@ -1,3 +1,5 @@
+start_trees
+
 points = 100;
 x = rand(points, 1) * 100;
 y = rand(points, 1) * 100;
@@ -9,13 +11,15 @@ mst_tree = MST_tree(1, [50; x], [50; y], [0;z], ...
 sp_tree = MST_tree(1, [50; x], [50; y], [0;z], ...
    1, 50, [], [], '-w');
 
-points = size(mst_tree.dA, 1);
-mst_set = set()
+[i, j, s] = find(mst_tree.dA);
+
+points = size(sp_tree, 1);
 arr = [];
 % depth first tour of MST
 for i=1:points
-    arr = [arr; mst_tree.dA(i,1)];
+    arr = [arr; cnct(i,1)];
 end
+
 
 S = 0;
 for i=1:(points - 1)
