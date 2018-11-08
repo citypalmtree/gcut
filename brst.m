f452bfd1b955f4d9768b10dc9baa903856a5e0fd
Karl Marrett <kdmarrett@gmail.com>
2018-11-07 19:13:11 -0800

    qfx

--- brst.m
+++ brst.m
@@ -1,6 +1,22 @@
-[adj_matrix] = brst(matrix)
+points = 100;
+x = rand(points, 1) * 100;
+y = rand(points, 1) * 100;
+z = zeros(points, 1) * 100;
 
-G = graph(matrix);
-mst = minspantree(G);
+mst_tree = MST_tree(1, [50; x], [50; y], [0;z], ...
+   0, 50, [], [], '-w');
 
-//L = depth first tour of mst;
+sp_tree = MST_tree(1, [50; x], [50; y], [0;z], ...
+   1, 50, [], [], '-w');
+
+points = size(mst_tree.dA, 1);
+mst_set = set()
+arr = [];
+% depth first tour of MST
+for i=1:points
+    arr = [arr; mst_tree.dA(i,1)];
+end
+
+S = 0;
+for i=1:(points - 1)
+end
